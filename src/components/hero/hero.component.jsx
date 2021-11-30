5ddf11d8ff35eb68a5a2a46e85184bbc6b11159a
pranta-barua007 <prantabarua247@gmail.com>
2021-11-30 13:57:24 +0600

    moved styled of hero in styled.jsx

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,94 +1,35 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import heroAvatar from "../../assets/avatar.png";
 
-const HeroInfo = styled(Paper)(({ theme }) => ({
-  color: "#fff",
-  height: "70vh",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "start",
-  textTransform: "uppercase",
-  background: "none",
-  boxShadow: "none",
-}));
+import {
+  HeroContainer,
+  HeroInfoContainer,
+  HeroAvatarContainer,
+  HeroCtaButton,
+  HeroSubTitle,
+  HeroTitle
+} from "./hero.styled";
 
 export default function Hero() {
   return (
-    <Box sx={{ flexGrow: 1, backgroundColor: "#a6a6a6", paddingTop: "1rem" }}>
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item sm={5} xs={12} md={5} lg={5}>
-            <HeroInfo sx={{ height: { xs: "85.5vh" } }}>
-              <Typography
-                variant="h2"
-                sx={{
-                  letterSpacing: "20px",
-                  fontSize: { xs: "1.6rem", sm: "2rem", lg: "5rem" },
-                }}
-              >
-                erin
-              </Typography>
-              <Typography
-                variant="h2"
-                sx={{
-                  letterSpacing: "20px",
-                  fontSize: { xs: "1.6rem", sm: "2rem", lg: "5rem" },
-                }}
-              >
-                krampitz
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                sx={{ textTransform: "capitalize", margin: "2rem 0" }}
-              >
-                Interior Decorator and Designer
-              </Typography>
-              <Button
-                variant="outlined"
-                endIcon={<ChevronRightIcon />}
-                sx={{
-                  marginTop: "2rem",
-                  paddingLeft: "2rem",
-                  paddingRight: "2rem",
-                  borderColor: "white",
-                  color: "white",
-                  "&:hover": {
-                    backgroundColor: "white",
-                    color: "#000",
-                    borderColor: "white",
-                  },
-                }}
-              >
-                HIRE ME NOW
-              </Button>
-            </HeroInfo>
-          </Grid>
-          <Grid item sm={7} xs={12} md={7} lg={7}>
-            <Typography
-              noWrap
-              component="div"
-              sx={{
-                mr: 2,
-                display: { xs: "none", sm: "flex", md: "flex" },
-                width: "auto",
-                height: { xs: "60vh", md: "88vh", lg: "110vh" },
-                justifyContent: "flex-end",
-              }}
-            >
-              <img src={heroAvatar} alt="hero-avatar" />
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+    <HeroContainer>
+      <HeroInfoContainer>
+        <HeroTitle>
+          erin
+        </HeroTitle>
+        <HeroTitle>
+          krampitz
+        </HeroTitle>
+        <HeroSubTitle>
+          Interior Decorator and Designer
+        </HeroSubTitle>
+        <HeroCtaButton>
+          HIRE ME NOW
+        </HeroCtaButton>
+      </HeroInfoContainer>
+      <HeroAvatarContainer>
+        <img src={heroAvatar} alt="hero-avatar" />
+      </HeroAvatarContainer>
+    </HeroContainer>
   );
 }
---
+++ src/components/hero/hero.styled.jsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+export const HeroContainer = ({ children }) => (
+  <Box flexGrow="1" paddingTop="1rem" backgroundColor="#a6a6a6">
+    <Container maxWidth="lg">
+      <Grid container spacing={2}>
+        {children}
+      </Grid>
+    </Container>
+  </Box>
+);
+
+export const HeroInfoContainer = ({ children }) => (
+  <Grid item sm={5} xs={12} md={5} lg={5}>
+    <HeroTextContainer sx={{ height: { xs: "85.5vh", sm: "94vh" } }}>
+      {children}
+    </HeroTextContainer>
+  </Grid>
+);
+
+export const HeroAvatarContainer = ({ children }) => (
+  <Grid item sm={7} xs={12} md={7} lg={7}>
+    <Box
+      sx={{
+        mr: 2,
+        display: { xs: "none", sm: "flex", md: "flex" },
+        width: "auto",
+        height: { xs: "60vh", md: "88vh", lg: "110vh" },
+        maxWidth: "800px",
+        justifyContent: "flex-end",
+      }}
+    >
+      {children}
+    </Box>
+  </Grid>
+);
+
+export const HeroTitle = ({ children }) => (
+  <Typography
+    variant="h2"
+    sx={{
+      letterSpacing: "20px",
+      fontSize: { xs: "1.6rem", sm: "2rem", lg: "5rem" },
+    }}
+  >
+    {children}
+  </Typography>
+);
+
+export const HeroSubTitle = ({ children }) => (
+  <Typography
+    variant="subtitle1"
+    sx={{ textTransform: "capitalize", margin: "2rem 0" }}
+  >
+    { children }
+  </Typography>
+);
+
+export const HeroCtaButton = ({ children, ...otherProps }) => (
+  <Button
+    variant="outlined"
+    endIcon={<ChevronRightIcon />}
+    sx={{
+      marginTop: "2rem",
+      paddingLeft: "2rem",
+      paddingRight: "2rem",
+      borderColor: "white",
+      color: "white",
+      "&:hover": {
+        backgroundColor: "white",
+        color: "#000",
+        borderColor: "white",
+      },
+    }}
+    {...otherProps}
+  >
+    {children}
+  </Button>
+);
+
+//should we stick to styled or sx?
+const HeroTextContainer = styled(Paper)(() => ({
+  color: "#fff",
+  height: "70vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "start",
+  textTransform: "uppercase",
+  background: "none",
+  boxShadow: "none",
+}));
